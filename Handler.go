a6d1a6076b728b14c575947864bdebde5dfd2f06
maximediet <maxime.diet76@gmail.com>
2023-01-19 11:18:53 +0100

    v1.2

--- Handler.go
+++ Handler.go
@@ -7,17 +7,17 @@ import (
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	t := template.New("home")
-	t, _ = t.ParseFiles("templates/home.html", "./templates/static/header.html", "./templates/static/footer.html", "./templates/static/ws.html")
+	t, _ = t.ParseFiles("templates/home.html", "./templates/static/header.html", "./templates/static/footer.html", "./templates/static/ws.html", "./templates/static/js.html")
 	t.ExecuteTemplate(w, "home", nil)
 }
 func loginPage(w http.ResponseWriter, r *http.Request) {
 	t := template.New("login")
-	t, _ = t.ParseFiles("templates/login.html", "./templates/static/header.html", "./templates/static/footer.html", "./templates/static/ws.html")
+	t, _ = t.ParseFiles("templates/login.html", "./templates/static/header.html", "./templates/static/footer.html", "./templates/static/ws.html", "./templates/static/js.html")
 	t.ExecuteTemplate(w, "login", nil)
 }
 
 func registerPage(w http.ResponseWriter, r *http.Request) {
 	t := template.New("register")
-	t, _ = t.ParseFiles("templates/register.html", "./templates/static/header.html", "./templates/static/footer.html", "./templates/static/ws.html")
+	t, _ = t.ParseFiles("templates/register.html", "./templates/static/header.html", "./templates/static/footer.html", "./templates/static/ws.html", "./templates/static/js.html")
 	t.ExecuteTemplate(w, "register", nil)
 }
