511a61a4b462a913441bf1142c3c5e66b0bb5350
Ulrik <ulrik.edeline@outlook.fr>
2023-01-25 16:50:56 +0100

    New cookie username and page reload

---
+++ static/js/front.js
@@ -0,0 +1,8 @@
+function getCookie(name) {
+    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+    return match ? match[1] : null;
+}
+
+let username = getCookie("username")
+document.getElementById('user').innerText = username;
