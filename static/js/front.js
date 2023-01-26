478a5cce4423455c56b0c3d61c05832a7f0d154e
Ulrik <ulrik.edeline@outlook.fr>
2023-01-26 16:43:22 +0100

    File deletion

--- static/js/front.js
+++
@@ -1,8 +0,0 @@
-function getCookie(name) {
-    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
-    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
-    return match ? match[1] : null;
-}
-
-let username = getCookie("username")
-document.getElementById('user').innerText = username;
