70f7aa8d8435ae4066093450cd0472696764955b
Ulrik <ulrik.edeline@outlook.fr>
2023-02-16 23:54:13 +0100

    Fixed console error messages, arranged code, added backend to the post form and a post loading function

--- static/js/views/index.mjs
+++ static/js/views/index.mjs
@@ -5,7 +5,27 @@ export default {
         <input id="post_categories" type="text" placeholder="Post Categories">
         <input id="post_content" type="text" placeholder="Post Content">
         <button id="post_button">Créer</button>
+        </div>
+        <div id="postList">
         </div>`;
     },
-    postRender: () => { },
+    postRender: () => { 
+        document.querySelector('#post_button').addEventListener('click', function() {
+            let title = document.querySelector('#post_title').value;
+            let content = document.querySelector('#post_content').value;
+            let categories = document.querySelector('#post_categories').value;
+            if (title != "" && content != "" && categories != "") {
+                CreatePost(title, content, categories);
+            }
+            document.querySelector('#post_title').value = "";
+            document.querySelector('#post_content').value = "";
+            document.querySelector('#post_categories').value = "";
+            if (document.querySelectorAll('.success').length > 0) {
+                let success = document.createElement('p');
+                success.innerHTML = "Post created";
+                success.classList.add('successMessage');
+                document.querySelector('#postcreator').appendChild(success);
+            }
+        });
+    },
 }
