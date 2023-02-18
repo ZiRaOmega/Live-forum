157c6c804d0a1b7b227d9c3509af7d9b876d7fc3
Snail Factory <>
2023-02-18 04:33:46 +0100

    send pm on enter and show last message

--- static/js/views/pm.mjs
+++ static/js/views/pm.mjs
@@ -1,31 +1,36 @@
 export default {
-    render: () => {
-        return `<div class="convHolder">
+  render: () => {
+    return `<div class="convHolder">
         <div class="conv">
         </div>
         <div id="currentDiscussion"></div>
+        <form id="form-send-message">
         <input id="sender" type="text" placeholder="Send">
-        <button id="sender_button">Envoyer</button>
+        <input type="submit" id="sender_button" value="Envoyer"></input>
+        </form>
         </div>
         <div class="recentconv">
         <div id="recent">Recent</div>
         <div class="convs">
         <div>
         </div>`;
-    },
-    postRender: () => {
-        document.querySelector('#sender_button').addEventListener('click', () => {
-            const field = document.querySelector('#sender');
-            let text = field.value;
-            if (text != "") {
-                field.value = "";
-                
-                text = text.replaceAll("$", "🐧");
-            
-                const recipient = currentDiscussion;
-                sendPrivateMessage(text, recipient);
-            }
-        });
-        
-    },
-}
+  },
+  postRender: () => {
+    document
+      .querySelector("#form-send-message")
+      .addEventListener("submit", (ev) => {
+        ev.preventDefault();
+
+        const field = document.querySelector("#sender");
+        let text = field.value;
+        if (text != "") {
+          field.value = "";
+
+          text = text.replaceAll("$", "🐧");
+
+          const recipient = currentDiscussion;
+          sendPrivateMessage(text, recipient);
+        }
+      });
+  },
+};
